9837cce1d209219c68a41aca618bec9cde8d5045
Stavros Kontopoulos <stavros.kontopoulos@lightbend.com>
2017-12-02 01:46:01 +0200

    modify producer

--- big-data-italy-2017/demo/rsvp-producer/src/RSVP_Producer.py
+++ big-data-italy-2017/demo/rsvp-producer/src/RSVP_Producer.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import requests
 import os
+from os import sys
 from confluent_kafka import Producer
 from pprint import pprint
 
@@ -9,13 +10,17 @@ def produce_rsvps(bootstrap_servers, topic):
     """Write full rsvp json objects to a kafka topic"""
     rsvps_stream = requests.get('http://stream.meetup.com/2/rsvps', stream=True)
     kafka_producer = Producer({'bootstrap.servers': bootstrap_servers})
-    for raw_rsvp in rsvps_stream.iter_lines():
-        if raw_rsvp:
-            rsvp = json.loads(raw_rsvp)
-            if rsvp["response"] == "yes":
-                pprint(rsvp)
-                kafka_producer.produce(topic, json.dumps(rsvp).encode('utf-8'))
-                kafka_producer.flush()
+    while True:
+        try:
+            for raw_rsvp in rsvps_stream.iter_lines():
+                if raw_rsvp:
+                    rsvp = json.loads(raw_rsvp)
+                    if rsvp["response"] == "yes":
+                        pprint(rsvp)
+                        kafka_producer.produce(topic, json.dumps(rsvp).encode('utf-8'))
+                        kafka_producer.flush()
+        except requests.exceptions.ChunkedEncodingError as e:
+            print e
 
 def main():
     parser = argparse.ArgumentParser()
