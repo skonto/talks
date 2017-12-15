9853f66d82f09193684df5f3c85552ef7d06838e
Stavros Kontopoulos <stavros.kontopoulos@lightbend.com>
2017-12-16 01:36:36 +0200

    fix synch producer in python

--- big-data-italy-2017/demo/rsvp-producer/src/RSVP_Producer.py
+++ big-data-italy-2017/demo/rsvp-producer/src/RSVP_Producer.py
@@ -9,18 +9,21 @@ from pprint import pprint
 def produce_rsvps(bootstrap_servers, topic):
     """Write full rsvp json objects to a kafka topic"""
     rsvps_stream = requests.get('http://stream.meetup.com/2/rsvps', stream=True)
-    kafka_producer = Producer({'bootstrap.servers': bootstrap_servers})
+    kafka_producer = Producer({'bootstrap.servers': bootstrap_servers, 'queue.buffering.max.ms': 10})
     while True:
         try:
             for raw_rsvp in rsvps_stream.iter_lines():
                 if raw_rsvp:
                     rsvp = json.loads(raw_rsvp)
                     if rsvp["response"] == "yes":
-                        pprint(rsvp)
                         kafka_producer.produce(topic, json.dumps(rsvp).encode('utf-8'))
-                        kafka_producer.flush()
         except requests.exceptions.ChunkedEncodingError as e:
             print e
+            break
+        except:
+            print "unexpected error"
+            raise
+    kafka_producer.flush()
 
 def main():
     parser = argparse.ArgumentParser()
